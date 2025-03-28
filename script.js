12fdd79cf0740cf25e0eed3271ad983c4c3a4015
RichardPitoniak <richard106285@student-schoolsofwestfield.org>
2025-03-28 14:56:50 +0000

    save

--- script.js
+++ script.js
@@ -4,31 +4,36 @@
  * FLOWCHART: https://lucid.app/lucidchart/5a3164fd-459f-494d-9cae-b4a6be593b13/view
  */
 
+main()
 
-/* main controls the program. Calling askQuestions() provides feedback depending on the 
- * number right returned: Either "Perfect!" or it says how many right out of the number asked. 
- * Store the number of questions to ask in a variable called questions.
+/* main controls the program. Calling askQuestions() it provides feedback depending on the 
+ * number right returned: Either "Perfect!" otherwise it says how many right out of the number asked. 
+ * set the number of questions as the variable questions 
  * @param none
  * @return none
  */
 function main() {
-
+let questions = 5 
+let right = askQuestions(questions)
+if (right == questions) { alert ("perfect")}
+else alert (" you got " + right + " out of " + questions)
 }
-
-/* askQuestions calls askQuestion() question number if times, sending the question number as an argument. 
- * It counts the number right returned, and return number right to main().
+/* askFive calls askQuestion() question times, sending the question number as an argument. 
+ * It counts number wrong returned, and return number wrong to main().
  * @param: none
- * @return: {integer} score (0-questions)
+ * @return: score (0-questions)
  */
 function askQuestions() {
 
+
+
 }
 
-/* askQuestion asks a multiplication question, using the question parameter to say which
+/* askQuestion asks a multiplication question, using the quesiton parameter to say which
  * question is being asked.  It returns 1 if correct, 0 if incorrect.
- * @param: {integer} question 
- * @return: {integer} (0 or 1) or {boolean}
+ * @param: question (integer 1-5)
+ * @return: integer (0 or 1)
  */
 function askQuestion(question){
-  
+ 
 }
