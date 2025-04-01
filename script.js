3e14ab915c57b06515f62221eee7ad0dc7968c42
RichardPitoniak <richard106285@student-schoolsofwestfield.org>
2025-04-01 10:56:32 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,36 +4,51 @@
  * FLOWCHART: https://lucid.app/lucidchart/5a3164fd-459f-494d-9cae-b4a6be593b13/view
  */
 
-main()
 
-/* main controls the program. Calling askQuestions() it provides feedback depending on the 
- * number right returned: Either "Perfect!" otherwise it says how many right out of the number asked. 
- * set the number of questions as the variable questions 
+/* main controls the program. Calling askQuestions() provides feedback depending on the 
+ * number right returned: Either "Perfect!" or it says how many right out of the number asked. 
+ * Store the number of questions to ask in a variable called questions.
  * @param none
  * @return none
  */
 function main() {
-let questions = 5 
-let right = askQuestions(questions)
-if (right == questions) { alert ("perfect")}
-else alert (" you got " + right + " out of " + questions)
+let questions = 5
+let right = askQuestion(questions)
+if (right == questions) alert ("Perfect!")
+else alert ("you got " + right + " out of " + questions)
 }
-/* askFive calls askQuestion() question times, sending the question number as an argument. 
- * It counts number wrong returned, and return number wrong to main().
+
+/* askQuestions calls askQuestion() question number if times, sending the question number as an argument. 
+ * It counts the number right returned, and return number right to main().
  * @param: none
- * @return: score (0-questions)
+ * @return: {integer} score (0-questions)
  */
 function askQuestions() {
-
-
-
+let right = 0
+let question = 1
+while (question <= questions ) {
+  score += askQuestion(questions)
+questions++
+}
+right
 }
 
-/* askQuestion asks a multiplication question, using the quesiton parameter to say which
+/* askQuestion asks a multiplication question, using the question parameter to say which
  * question is being asked.  It returns 1 if correct, 0 if incorrect.
- * @param: question (integer 1-5)
- * @return: integer (0 or 1)
+ * @param: {integer} question 
+ * @return: {integer} (0 or 1) or {boolean}
  */
 function askQuestion(question){
- 
+  //L-H= Math.random()*(H-L+1)+L;
+  let a = Math.floor(Math.random()*9-3+1)+3
+  let b = Math.floor(Math.random()*9-3+1)+3
+  let product = a*b
+  let equations = "question " + question + ": " + a + " * " + b + " =?"
+  let answer = prompt(equations)
+  if (answer == product) {
+    alert ("correct")
+    return true 
+  }
+else alert ("incorrect")
+return false 
 }
